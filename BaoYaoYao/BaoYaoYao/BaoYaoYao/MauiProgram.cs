f519131c6a46dc8dce38f03deb8ba9aedf56da74
Vulcan <vulcan.lee@gmail.com>
2022-09-28 20:21:05 +0800

    還原使用 "Prism.DryIoc.Maui" Version="8.1.273-pre"

--- BaoYaoYao/BaoYaoYao/BaoYaoYao/MauiProgram.cs
+++ BaoYaoYao/BaoYaoYao/BaoYaoYao/MauiProgram.cs
@@ -12,32 +12,32 @@ public static class MauiProgram
     {
         var builder = MauiApp.CreateBuilder();
         builder
-            .UsePrismApp<App>(PrismStartup.Configure)
+            //.UsePrismApp<App>(PrismStartup.Configure)
 
-            //.UseMauiApp<App>()
-            //.UsePrism(prism =>
-            //{
-            //    prism.OnAppStart("SplashPage");
+            .UseMauiApp<App>()
+            .UsePrism(prism =>
+            {
+                prism.OnAppStart("SplashPage");
 
-            //    prism.RegisterTypes(container =>
-            //    {
-            //        container.RegisterForNavigation<MainPage>()
-            //                     .RegisterInstance(SemanticScreenReader.Default);
-            //        container.RegisterForNavigation<SplashPage, SplashPageViewModel>();
-            //        container.RegisterForNavigation<LoginPage, LoginPageViewModel>();
-            //        container.RegisterForNavigation<RegistrationPage, RegistrationPageViewModel>();
-            //        container.RegisterForNavigation<NaviPage, NaviPageViewModel>();
-            //        container.RegisterForNavigation<VerifyPhonePage, VerifyPhonePageViewModel>();
-            //        container.RegisterForNavigation<ConnectPharmacyPage, ConnectPharmacyPageViewModel>();
-            //        container.RegisterForNavigation<ApplyPage, ApplyPageViewModel>();
-            //        container.RegisterForNavigation<ApplyHistoryPage, ApplyHistoryPageViewModel>();
-            //    });
+                prism.RegisterTypes(container =>
+                {
+                    container.RegisterForNavigation<MainPage>()
+                                 .RegisterInstance(SemanticScreenReader.Default);
+                    container.RegisterForNavigation<SplashPage, SplashPageViewModel>();
+                    container.RegisterForNavigation<LoginPage, LoginPageViewModel>();
+                    container.RegisterForNavigation<RegistrationPage, RegistrationPageViewModel>();
+                    container.RegisterForNavigation<NaviPage, NaviPageViewModel>();
+                    container.RegisterForNavigation<VerifyPhonePage, VerifyPhonePageViewModel>();
+                    container.RegisterForNavigation<ConnectPharmacyPage, ConnectPharmacyPageViewModel>();
+                    container.RegisterForNavigation<ApplyPage, ApplyPageViewModel>();
+                    container.RegisterForNavigation<ApplyHistoryPage, ApplyHistoryPageViewModel>();
+                });
 
-            //    //prism.ConfigureServices(container =>
-            //    //{
-            //    //    container.RegisterForNavigation<MainPage>();
-            //    //});
-            //})
+                //prism.ConfigureServices(container =>
+                //{
+                //    container.RegisterForNavigation<MainPage>();
+                //});
+            })
             .UseMauiCommunityToolkitMarkup()
             .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
