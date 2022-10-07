b87010ade96213bd6d647c0451b57c8bd4cf3943
Vulcan <vulcan.lee@gmail.com>
2022-10-07 23:14:25 +0800

    WPF MVVM

---
+++ wpfMVVM/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+
+namespace ViewModels
+{
+
+    public partial class MainPageViewModel : ObservableObject
+    {
+        public MainPageViewModel()
+        {
+            MyDataItems = new List<MyDataItem>()
+            {
+                new MyDataItem()
+                {
+                    Name = "sjflskjfslj",
+                    IsSpecial = false,
+                },
+                new MyDataItem()
+                {
+                    Name = "sjflskjfsljsjflskjfsljsjflskjfsljsjflskjfsljsjflskjfslj",
+                    IsSpecial = false,
+                },
+                new MyDataItem()
+                {
+                    Name = "sjflskjfsljsjflskjfsljsjflskjfsljsjflskjfsljsjflskjfslj",
+                    IsSpecial = true,
+                },
+            };
+        }
+
+        [ObservableProperty]
+        List<MyDataItem> myDataItems;
+
+
+    }
+}
---
+++ wpfMVVM/wpfMVVM/MyDataItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace wpfMVVM
+{
+    public class MyDataItem
+    {
+        public string Name { get; set; } = "";
+        public Visibility IsSpecial { get; set; } = Visibility.Collapsed;
+    }
+}
