f34cbb4782cf7d81008854fb3b17bf914c02da0b
Vulcan <vulcan.lee@gmail.com>
2022-10-17 13:46:11 +0800

    升級套件與修正最新版 prism.maui

--- NckuhRepair/NckuhRepair/NckuhRepair/MauiProgram.cs
+++ NckuhRepair/NckuhRepair/NckuhRepair/MauiProgram.cs
@@ -1,4 +1,8 @@
-﻿namespace NckuhRepair;
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Markup;
+using NckuhRepair.Views;
+
+namespace NckuhRepair;
 
 public static class MauiProgram
 {
@@ -6,7 +10,25 @@ public static class MauiProgram
     {
         var builder = MauiApp.CreateBuilder();
         builder
-            .UsePrismApp<App>(PrismStartup.Configure)
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkitMarkup()
+            .UseMauiCommunityToolkit()
+            .UsePrism(prism =>
+            {
+                prism.OnAppStart("MainPage");
+
+                prism.RegisterTypes(containerRegistry =>
+                {
+                    containerRegistry.RegisterForNavigation<MainPage>()
+                    .RegisterInstance(SemanticScreenReader.Default);
+
+                });
+
+                //prism.ConfigureServices(container =>
+                //{
+                //    container.RegisterForNavigation<MainPage>();
+                //});
+            })
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
