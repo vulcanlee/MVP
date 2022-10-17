b345b63edbda4f725af3b0a132b445f3a724646c
Vulcan <vulcan.lee@gmail.com>
2022-10-17 11:44:18 +0800

    建立叫修專案

---
+++ RepairService/RepairService/RepairService/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RepairService;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+}
---
+++ RepairService/RepairService/RepairService/Views/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RepairService.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
+
