749205cf2ea4a311b6bae983bdef880e876ac8fb
Vulcan <vulcan.lee@gmail.com>
2022-11-07 09:30:43 +0800

    FormItemService 可以儲存與取回所有表單紀錄

---
+++ NckuhRepair/NckuhRepair/NckuhRepair/Helpers/Utilities/UtilityHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NckuhRepair.Helpers.Utilities
+{
+    public class UtilityHelper
+    {
+        /// <summary>
+        /// Gets if there is an active internet connection
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsConnected()
+        {
+            var current = Connectivity.NetworkAccess;
+
+            if (current == NetworkAccess.Internet)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ NckuhRepair/NckuhRepair/NckuhRepair/Services/FormItemService.cs
@@ -0,0 +1,25 @@
+﻿using NckuhRepair.Helpers.WebAPIs;
+using NckuhRepair.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NckuhRepair.Services
+{
+    public class FormItemService : BaseWebAPI<List<FormIOModel>>
+    {
+        public FormItemService()
+            : base()
+        {
+            SetDefaultPersistentBehavior();
+        }
+
+        void SetDefaultPersistentBehavior()
+        {
+            ApiResultIsCollection = false;
+            PersistentStorage = ApiResultIsCollection ? PersistentStorage.Collection : PersistentStorage.Single;
+        }
+    }
+}
