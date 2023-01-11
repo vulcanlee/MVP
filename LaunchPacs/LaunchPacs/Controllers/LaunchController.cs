b0c918cc5986746e46e120bac36590044feea5bf
Vulcan <vulcan.lee@gmail.com>
2023-01-11 10:47:53 +0800

    可以啟動 PACS Viewer & 查詢版本編號

--- LaunchPacs/LaunchPacs/Controllers/LaunchController.cs
+++ LaunchPacs/LaunchPacs/Controllers/LaunchController.cs
@@ -1,4 +1,8 @@
+using LaunchPacs.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+using System.Xml.Linq;
 
 namespace LaunchPacs.Controllers
 {
@@ -7,24 +11,49 @@ namespace LaunchPacs.Controllers
     public class LaunchController : ControllerBase
     {
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<LaunchController> _logger;
 
-        public LaunchController(ILogger<WeatherForecastController> logger)
+        public LaunchController(ILogger<LaunchController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public string Get()
+        public string Get([FromQuery]G3LauncherModel g3LauncherModel)
         {
-            return "OK";
+            // C:\INFINITT\viewer\G3Launcher.exe http://10.1.1.142+admin+nimda++S+RU799OR39MJ3BCF1+BET0001
+            StringBuilder builderArgument= new StringBuilder();
+            builderArgument.Append($"{g3LauncherModel.Iis}+");
+            builderArgument.Append($"{g3LauncherModel.UserId}+");
+            builderArgument.Append($"{g3LauncherModel.UserPassword}+");
+            builderArgument.Append($"+S+");
+            builderArgument.Append($"{g3LauncherModel.AccessionNo}+");
+            builderArgument.Append($"{g3LauncherModel.PatientId}");
+            string arguments = builderArgument.ToString();
+
+            string command = $"{g3LauncherModel.ViewerPath} {arguments}";
+
+            // Prepare the process to run
+            ProcessStartInfo start = new ProcessStartInfo();
+            // Enter in the command line arguments, everything you would enter after the executable name itself
+            start.Arguments = arguments;
+            // Enter the executable to run, including the complete path
+            start.FileName = g3LauncherModel.ViewerPath;
+            // Do you want to show a console window?
+            //start.WindowStyle = ProcessWindowStyle.Hidden;
+            //start.CreateNoWindow = true;
+            using (Process proc = Process.Start(start))
+            {
+            }
+            return command;
         }
 
         [HttpGet(template:"Version")]
         public string GetVersion()
         {
             var version = new Program().GetType().Assembly.GetName().Version!.ToString();
-            return version;
+            var name = $"Exentric G3Launcher Support Tool (Version : {version})";
+            return name;
         }
     }
 }
---
+++ LaunchPacs/readme.md
@@ -0,0 +1,45 @@
+# G3Launch Gateway 說明
+
+# 呼叫端點參數說明
+
+若想要使用底下方式來開啟 PACS Viewer 
+
+C:\INFINITT\viewer\G3Launcher.exe http://10.1.1.142+admin+nimda++S+RU799OR39MJ3BCF1+BET0001
+
+這個時候，需要使用底下端點 URL 來呼叫
+
+其中，每個引數的字串值必須使用 URL Encoder 來進行編碼
+
+http://localhost:14928/Launch?ViewerPath=C%3A%5CINFINITT%5Cviewer%5CG3Launcher.exe&IIS=http%3A%2F%2F10.1.1.142&UserId=admin&UserPassword=nimda&AccessionNo=RU799OR39MJ3BCF1&PatientId=BET0001
+
+http://localhost:14928/Launch?ViewerPath=C%3A%5CINFINITT%5Cviewer%5CG3Launcher.exe&IIS=http%3A%2F%2F10.1.1.142&UserId=admin&UserPassword=nimda&AccessionNo=S21B2R1908569&PatientId=01658998
+
+## 參數說明
+
+* ViewerPath
+
+  PACS Viewer 的完整路徑
+
+* IIS
+
+  指向 PACS Web 主機的網址
+
+* UserId
+
+  帳號
+
+* UserPassword
+
+  密碼
+
+* AccessionNo
+
+  AccessionNo
+
+* PatientId
+
+  PatientId
+  
+# 測試方式
+
+參考 test.html 網站，並請使用任一瀏覽器打開該網址，實際操作與驗證
