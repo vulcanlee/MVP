1a3aaf221f24b31e10d3bfbb8d2b69ef852fdfb1
Vulcan <vulcan.lee@gmail.com>
2023-02-01 17:34:16 +0800

    加入 Maui 程式碼

---
+++ SignalrChat/SignalrClientMaui/SignalrClientMaui/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalrClientMaui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+}
---
+++ SignalrChat/SignalrClientMaui/SignalrClientMaui/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,55 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SignalrClientMaui.ViewModels;
+
+public partial class MainPageViewModel : ObservableObject, INavigatedAware
+{
+    #region Field Member
+    private int _count;
+    private readonly INavigationService navigationService;
+
+    [ObservableProperty]
+    string title = "Main Page";
+
+    [ObservableProperty]
+    string text = "Click me";
+    #endregion
+
+    #region Property Member
+    #endregion
+
+    #region Constructor
+    public MainPageViewModel(INavigationService navigationService)
+    {
+        this.navigationService = navigationService;
+    }
+    #endregion
+
+    #region Method Member
+    #region Command Method
+    [RelayCommand]
+    private void Count()
+    {
+        _count++;
+        if (_count == 1)
+            Text = "Clicked 1 time";
+        else if (_count > 1)
+            Text = $"Clicked {_count} times";
+    }
+    #endregion
+
+    #region Navigation Event
+    public void OnNavigatedFrom(INavigationParameters parameters)
+    {
+    }
+
+    public void OnNavigatedTo(INavigationParameters parameters)
+    {
+    }
+    #endregion
+
+    #region Other Method
+    #endregion
+    #endregion
+}
