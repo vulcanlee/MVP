40a6df3a34121f2154abb778f054bb3b48029ab9
Vulcan <vulcan.lee@gmail.com>
2023-02-01 18:11:42 +0800

    修正可以捕捉到斷線的例外異常

--- SignalrChat/SignalrClientMaui/SignalrClientMaui/ViewModels/MainPageViewModel.cs
+++ SignalrChat/SignalrClientMaui/SignalrClientMaui/ViewModels/MainPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace SignalrClientMaui.ViewModels;
 
@@ -8,7 +9,7 @@ public partial class MainPageViewModel : ObservableObject, INavigatedAware
     #region Field Member
     private int _count;
     private readonly INavigationService navigationService;
-
+    HubConnection connection;
     [ObservableProperty]
     string title = "Main Page";
 
@@ -22,6 +23,12 @@ public partial class MainPageViewModel : ObservableObject, INavigatedAware
     #region Constructor
     public MainPageViewModel(INavigationService navigationService)
     {
+        #region 建立與產生 SignalR Client 端的物件
+        connection = new HubConnectionBuilder()
+           .WithUrl("https://localhost:7181/myChatHub")
+           .Build();
+        #endregion
+
         this.navigationService = navigationService;
     }
     #endregion
@@ -44,12 +51,75 @@ public partial class MainPageViewModel : ObservableObject, INavigatedAware
     {
     }
 
-    public void OnNavigatedTo(INavigationParameters parameters)
+    public async void OnNavigatedTo(INavigationParameters parameters)
     {
+        BindingEvent();
+
+        await StartSignalRConnectionAsync();
+        await SendMessage();
     }
+
     #endregion
 
     #region Other Method
+    private async Task SendMessage()
+    {
+        #region 開始對 SignalR 伺服器送出訊息
+        try
+        {
+            await connection.InvokeAsync("SendMessage",
+                "Vulcan", "Hello everyone");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        #endregion
+
+        for (int i = 0; i < 10; i++)
+        {
+            await Task.Delay(500);
+        }
+    }
+
+    private async Task StartSignalRConnectionAsync()
+    {
+        #region 啟動與後端 SignalR 伺服器進行連線
+        try
+        {
+            await connection.StartAsync();
+            Console.WriteLine("Connection started");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        #endregion
+    }
+
+    private void BindingEvent()
+    {
+        #region 綁定相關事件
+        #region 網路發生異常，導致與後端 SignalR 伺服器連線中斷時機，將會觸發此事件
+        connection.Closed += async (error) =>
+        {
+            await Task.Delay(new Random().Next(0, 5) * 1000);
+            await MainThread.InvokeOnMainThreadAsync(async () =>
+             {
+                 await connection.StartAsync();
+             });
+        };
+        #endregion
+
+        #region 綁定有特定訊息要被發送出來的時機，將會觸發此事件
+        connection.On<string, string>("ReceiveMessage", (user, message) =>
+        {
+            var newMessage = $"{user}: {message}";
+            Console.WriteLine(newMessage);
+        });
+        #endregion
+        #endregion
+    }
     #endregion
     #endregion
 }
