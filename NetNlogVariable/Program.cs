e8d7539f00a8378b6eeff8f3a7bb30fce069197f
Vulcan <vulcan.lee@gmail.com>
2023-03-15 17:15:26 +0800

    改成這樣用法 ${var:DetectLogLevel}

--- NetNlogVariable/Program.cs
+++ NetNlogVariable/Program.cs
@@ -27,23 +27,10 @@ namespace NetNlog
             var config = LogManager.Configuration;
 
             // 設定要變更的變數值
-            config.Variables["myVar"] = "myValue";
             config.Variables["DetectLogLevel"] = $"trace";
 
             // 變更完畢後，重新載入 NLog 設定
             LogManager.Configuration = config;
-
-            // 使用變更後的變數值進行 logging
-            logger.Info("The value of myVar is ${var:myVar}");
-
-
-            //var config = LogManager.Configuration;
-            //config.Variables["DetectLogLevel"] = $"trace";
-            //LogManager.Configuration = config;
-            //// 立即重新載入設定
-            ////LogManager.ReconfigExistingLoggers();
-
-            //logger.Info("My variable is ${DetectLogLevel}");
         }
 
         private static void 使用預設變數LogLevel()
