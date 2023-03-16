1ed0e23b919136e0b010385fa8cfab169c8e3930
Vulcan <vulcan.lee@gmail.com>
2023-03-16 09:58:29 +0800

    測試 final="true"

---
+++ NetNlogRule/NetNlogRule/Program.cs
@@ -0,0 +1,32 @@
+﻿using MyNamespace.Database;
+using NLog;
+
+namespace NetNlogRule
+{
+    internal class Program
+    {
+        public static Logger logger =
+            LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            SomeClass someClass = new SomeClass();
+
+            WriteAllLog("經常使用的 Log 輸出");
+            someClass.WriteAllLog("使用 工程模式的 Log 輸出");
+        }
+
+        private static void WriteAllLog(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(message);
+            logger.Trace("我是追蹤:Trace");
+            logger.Debug("我是偵錯:Debug");
+            logger.Info("我是資訊:Info");
+            logger.Warn("我是警告:Warn");
+            logger.Error("我是錯誤:error");
+            logger.Fatal("我是致命錯誤:Fatal");
+        }
+    }
+}
---
+++ NetNlogRule/NetNlogRule/SomeClass.cs
@@ -0,0 +1,26 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace.Database
+{
+    public class SomeClass
+    {
+        Logger logger = LogManager.GetCurrentClassLogger();
+        public void WriteAllLog(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(message);
+            logger.Trace("我是追蹤 DB :Trace");
+            logger.Debug("我是偵錯 DB :Debug");
+            logger.Info("我是資訊 DB :Info");
+            logger.Warn("我是警告 DB :Warn");
+            logger.Error("我是錯誤 DB :error");
+            logger.Fatal("我是致命錯誤 DB :Fatal");
+        }
+    }
+}
