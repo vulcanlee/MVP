cac1b878b3d4170921eea5a3f767e494dc67558d
Vulcan <vulcan.lee@gmail.com>
2023-03-18 10:43:10 +0800

    啟用工程模式

---
+++ NLogSample/NLogSample/Helpers/LoggerHelper.cs
@@ -0,0 +1,70 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogSample.Helpers
+{
+    public class LoggerHelper
+    {
+        private EngineerModeCodeEnum engineerMode = EngineerModeCodeEnum.None;
+
+        public void SendLog(Action loggerHandler, EngineerModeCodeEnum engineerMode)
+        {
+            var actual = (this.engineerMode & engineerMode) == engineerMode;
+            if (actual) { loggerHandler(); }
+        }
+
+        /// <summary>
+        /// 加入某個工程模式
+        /// </summary>
+        /// <param name="engineerMode">指定之工程模式列舉值</param>
+        public void AddEngineerMode(EngineerModeCodeEnum engineerMode)
+        {
+            this.engineerMode = this.engineerMode | engineerMode;
+        }
+
+        /// <summary>
+        /// 移除某個工程模式
+        /// </summary>
+        /// <param name="engineerMode">指定之工程模式列舉值</param>
+        public void RemoveEngineerMode(EngineerModeCodeEnum engineerMode)
+        {
+            this.engineerMode = this.engineerMode & ~engineerMode;
+        }
+
+        /// <summary>
+        /// 強制設定某個工程模式
+        /// </summary>
+        /// <param name="engineerMode">指定之工程模式列舉值</param>
+        public void SetEngineerMode(EngineerModeCodeEnum engineerMode)
+        {
+            this.engineerMode = engineerMode;
+        }
+
+        /// <summary>
+        /// 清除所有工程模式
+        /// </summary>
+        /// <param name="engineerMode">指定之工程模式列舉值</param>
+        public void ClearEngineerMode()
+        {
+            this.engineerMode = EngineerModeCodeEnum.None;
+        }
+
+
+    }
+
+    [Flags]
+    public enum EngineerModeCodeEnum
+    {
+        None = 0,
+        登出登入 = 1,
+        資料庫存取 = 2,
+        讀卡機操作 = 4,
+        MQTT = 8,
+        呼叫WebAPI = 16,
+        All = 登出登入 | 資料庫存取 | 讀卡機操作 | MQTT | 呼叫WebAPI
+    }
+}
--- NLogSample/NLogSample/Program.cs
+++ NLogSample/NLogSample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using NLog;
+using NLogSample.Helpers;
 
 namespace NLogSample
 {
@@ -6,13 +7,54 @@ namespace NLogSample
     {
         public static Logger logger =
             LogManager.GetCurrentClassLogger();
+        public static LoggerHelper loggerHelper = new LoggerHelper();
         static void Main(string[] args)
         {
-            WriteAllLevelLog("使用預設變數LogLevel");
+            string 讀卡機操作工程模式用之日誌事件 = "讀卡機操作 工程模式用之日誌事件";
+            string 呼叫WebAPI工程模式用之日誌事件 = "呼叫WebAPI 工程模式用之日誌事件";
+            模擬產生相關日誌事件("使用預設變數LogLevel",
+                讀卡機操作工程模式用之日誌事件, 呼叫WebAPI工程模式用之日誌事件);
+
             啟用工程模式();
-            WriteAllLevelLog("啟用 工程模式");
+            模擬產生相關日誌事件("啟用 工程模式",
+                讀卡機操作工程模式用之日誌事件, 呼叫WebAPI工程模式用之日誌事件);
+
             關閉工程模式();
-            WriteAllLevelLog("關閉 工程模式");
+            模擬產生相關日誌事件("關閉 工程模式",
+                讀卡機操作工程模式用之日誌事件, 呼叫WebAPI工程模式用之日誌事件);
+
+            啟用工程模式且僅限讀卡機操作();
+            模擬產生相關日誌事件("啟用工程模式且僅限讀卡機操作",
+                讀卡機操作工程模式用之日誌事件, 呼叫WebAPI工程模式用之日誌事件);
+
+            啟用工程模式且指定呼叫WebAPI與讀卡機操作();
+            模擬產生相關日誌事件("啟用工程模式且指定呼叫WebAPI與讀卡機操作",
+                讀卡機操作工程模式用之日誌事件, 呼叫WebAPI工程模式用之日誌事件);
+
+            啟用工程模式且沒有指定任何條件();
+            模擬產生相關日誌事件("啟用工程模式且沒有指定任何條件",
+                讀卡機操作工程模式用之日誌事件, 呼叫WebAPI工程模式用之日誌事件);
+        }
+
+        private static void 啟用工程模式且沒有指定任何條件()
+        {
+            啟用工程模式();
+            loggerHelper.ClearEngineerMode();
+        }
+
+        private static void 啟用工程模式且指定呼叫WebAPI與讀卡機操作()
+        {
+            啟用工程模式();
+            loggerHelper.ClearEngineerMode();
+            loggerHelper.SetEngineerMode(EngineerModeCodeEnum.讀卡機操作);
+            loggerHelper.AddEngineerMode(EngineerModeCodeEnum.呼叫WebAPI);
+        }
+
+        private static void 啟用工程模式且僅限讀卡機操作()
+        {
+            啟用工程模式();
+            loggerHelper.ClearEngineerMode();
+            loggerHelper.SetEngineerMode(EngineerModeCodeEnum.讀卡機操作);
         }
 
         private static void 啟用工程模式()
@@ -39,6 +81,13 @@ namespace NLogSample
             LogManager.Configuration = config;
         }
 
+        private static void 模擬產生相關日誌事件(string message1, string message2, string message3)
+        {
+            WriteAllLevelLog(message1);
+            WriteAll讀卡機操作Log(message2);
+            WriteAll呼叫WebAPILog(message3);
+        }
+
         private static void WriteAllLevelLog(string message)
         {
             Console.WriteLine();
@@ -50,6 +99,52 @@ namespace NLogSample
             logger.Warn("我是警告:Warn");
             logger.Error("我是錯誤:error");
             logger.Fatal("我是致命錯誤:Fatal");
+        }
+
+        private static void WriteAll呼叫WebAPILog(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(message);
+            loggerHelper.SendLog(() =>
+            {
+                logger.Trace("呼叫WebAPI 追蹤:Trace");
+            }, EngineerModeCodeEnum.呼叫WebAPI);
+            loggerHelper.SendLog(() =>
+            {
+                logger.Debug("呼叫WebAPI 我是偵錯:Debug");
+            }, EngineerModeCodeEnum.呼叫WebAPI);
+            loggerHelper.SendLog(() =>
+            {
+                logger.Info("呼叫WebAPI 我是資訊:Info");
+            }, EngineerModeCodeEnum.呼叫WebAPI);
+
+            logger.Warn("呼叫WebAPI 我是警告:Warn");
+            logger.Error("呼叫WebAPI 我是錯誤:error");
+            logger.Fatal("呼叫WebAPI 我是致命錯誤:Fatal");
+        }
+
+        private static void WriteAll讀卡機操作Log(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(message);
+            loggerHelper.SendLog(() =>
+            {
+                logger.Trace("讀卡機操作 追蹤:Trace");
+            }, EngineerModeCodeEnum.讀卡機操作);
+            loggerHelper.SendLog(() =>
+            {
+                logger.Debug("讀卡機操作 我是偵錯:Debug");
+            }, EngineerModeCodeEnum.讀卡機操作);
+            loggerHelper.SendLog(() =>
+            {
+                logger.Info("讀卡機操作 我是資訊:Info");
+            }, EngineerModeCodeEnum.讀卡機操作);
+
+            logger.Warn("讀卡機操作 我是警告:Warn");
+            logger.Error("讀卡機操作 我是錯誤:error");
+            logger.Fatal("讀卡機操作 我是致命錯誤:Fatal");
         }
     }
 }
