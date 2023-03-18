26295a31451d660c8f3424c74fcb060f5a86e81f
Vulcan <vulcan.lee@gmail.com>
2023-03-18 22:12:27 +0800

    NLog Benchmark

---
+++ NLogBenchmark/NLogBenchmark/NLogBenchmarkHelper.cs
@@ -0,0 +1,19 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogBenchmark
+{
+    public static class NLogBenchmarkHelper
+    {
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public static void WriteLog()
+        {
+            logger.Info("This is a log message.");
+        }
+    }
+}
---
+++ NLogBenchmark/NLogBenchmark/Program.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using NLog;
+
+namespace NLogBenchmark
+{
+    public class NLogBenchmarkTests
+    {
+        [Benchmark]
+        public void TestWriteLog()
+        {
+            NLogBenchmarkHelper.WriteLog();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<NLogBenchmarkTests>();
+        }
+    }
+}
